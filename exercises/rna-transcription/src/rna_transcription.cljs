da24a91aa2ae3f06ae9ce63970de17f55a51c14c
LeaveNhA <seckin.kukrer@hotmail.com>
2020-08-09 18:00:19 +0300

    Exercise file is fixed. Example source file added.

--- exercises/rna-transcription/src/rna_transcription.cljs
+++ exercises/rna-transcription/src/rna_transcription.cljs
@@ -1,14 +1,5 @@
 (ns rna-transcription)
 
-(def ^:private
-  dna->rna {\G \C
-            \C \G
-            \A \U
-            \T \A})
-
-(defn- translate [c]
-  {:post [%]}
-  (dna->rna c))
-
-(defn to-rna [dna]
-  (apply str (map translate dna)))
+(defn to-rna [] ;; <--- arguments goes here...
+  ;; your code goes here...
+  )
