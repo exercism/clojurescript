baaa3f01570cdd0c229d8da52cfbc75a93d7d384
LeaveNhA <seckin.kukrer@hotmail.com>
2020-08-09 22:05:19 +0300

    Clock exercise implemented...

---
+++ exercises/clock/src/example.cljs
@@ -0,0 +1,21 @@
+(ns clock
+  (:require [goog.string :as gstring]
+            [goog.string.format]))
+
+(defn clock
+  "Return a 24 hour clock representation of the given hours and minutes."
+  [in-hour in-minute]
+  (let [total-minutes (mod (+ (* in-hour 60) in-minute) (* 60 24))
+        hours (mod (quot total-minutes 60) 24)
+        minutes (mod total-minutes 60)]
+    {:hour hours :minute minutes}))
+
+(defn clock->string
+  "Print the HH:MM representation of a clock."
+  [in-clock]
+  (gstring/format "%02d:%02d" (:hour in-clock) (:minute in-clock)))
+
+(defn add-time
+  "Add minutes to the given clock."
+  [in-clock minutes-to-add]
+  (clock (:hour in-clock) (+ (:minute in-clock) minutes-to-add)))
