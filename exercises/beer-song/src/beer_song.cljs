fc22661f88069d171bf03a309d7f160b2fe7726d
LeaveNhA <seckin.kukrer@hotmail.com>
2020-08-09 18:59:18 +0300

    Beer Song fixed.

--- exercises/beer-song/src/beer_song.cljs
+++ exercises/beer-song/src/beer_song.cljs
@@ -1,24 +1,9 @@
-(ns beer-song
-  (:require [clojure.string :as str]
-            [goog.string :as gstring]
-            [goog.string.format]))
+(ns beer-song)
 
-(defn many [n]
-  (gstring/format "%d bottles of beer on the wall, %d bottles of beer.\nTake one down and pass it around, %d bottles of beer on the wall.\n" n n (dec n)))
+(defn verse [] ;; <-- arguments goes here...
+  ;; your code goes here...
+  )
 
-(def two "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n")
-(def one "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n")
-(def zero "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n")
-
-(defn verse [v]
-  (case (int v)
-    0 zero
-    1 one
-    2 two
-    (many v)))
-
-(defn sing
-  ([n]   (sing n 0))
-  ([n m] (->> (range n (dec m) -1)
-              (map verse)
-              (str/join "\n"))))
+(defn sing [] ;; <-- arguments goes here...
+  ;; your code goes here...
+  )
