f773a674ce5cefd0c5649646feacb3d9e61e99a3
LeaveNhA <seckin.kukrer@hotmail.com>
2020-08-11 18:24:32 +0300

    Re-factored RNA Transcription exercise tests.

--- exercises/rna-transcription/test/rna_transcription_test.cljs
+++ exercises/rna-transcription/test/rna_transcription_test.cljs
@@ -2,26 +2,26 @@
   (:require [cljs.test :as t :include-macros true]
             rna-transcription))
 
-(t/deftest transcribes-cytosine-to-guanine
+(t/deftest transcribes-cytosine-to-guanine-test
   (t/testing "Transcribes Cytosine to Guanine..."
     (t/is (= "G" (rna-transcription/to-rna "C")))))
 
-(t/deftest transcribes-guanine-to-cytosine
+(t/deftest transcribes-guanine-to-cytosine-test
   (t/testing "Transcribes Guanine to Cytosine..."
     (t/is (= "C" (rna-transcription/to-rna "G")))))
 
-(t/deftest transcribes-adenine-to-uracil
+(t/deftest transcribes-adenine-to-uracil-test
   (t/testing "Transcribes Adenine to Uracil..."
     (t/is (= "U" (rna-transcription/to-rna "A")))))
 
-(t/deftest transcribes-thymine-to-adenine
+(t/deftest transcribes-thymine-to-adenine-test
   (t/testing "Transcribes Tymine to Adenine..."
     (t/is (= "A" (rna-transcription/to-rna "T")))))
 
-(t/deftest transcribes-all-nucleotides
+(t/deftest transcribes-all-nucleotides-test
   (t/testing "Transcribes all nucleotides..."
-    (t/is (= "UGCACCAGAAUU" (rna-transcription/to-rna "ACGTGGTCTTAA")))))
+    (t/is (= "CUGCUGCUGCUGCAAUCAUCGCAU" (rna-transcription/to-rna "GACGACGACGACGTTAGTAGCGTA")))))
 
-(t/deftest validates-dna-strands
+(t/deftest validates-dna-strands-test
   (t/testing "Transcribes DNA Strands..."
     (t/is (thrown? js/Error (rna-transcription/to-rna "XCGFGGTDTTAA")))))
