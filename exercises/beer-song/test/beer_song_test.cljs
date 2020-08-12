c99dc45fafcc5c9455c8bd4199224ff9cb361b27
LeaveNhA <seckin.kukrer@hotmail.com>
2020-08-12 20:15:59 +0300

    Removed the forgotten dead-codes from test source.

--- exercises/beer-song/test/beer_song_test.cljs
+++ exercises/beer-song/test/beer_song_test.cljs
@@ -2,24 +2,6 @@
   (:require [cljs.test :refer [deftest is are testing] :as t :include-macros true]
             beer-song))
 
-(def song-8-6
-  (str "8 bottles of beer on the wall, 8 bottles of beer.\n"
-       "Take one down and pass it around, 7 bottles of beer on the wall.\n\n"
-       "7 bottles of beer on the wall, 7 bottles of beer.\n"
-       "Take one down and pass it around, 6 bottles of beer on the wall.\n\n"
-       "6 bottles of beer on the wall, 6 bottles of beer.\n"
-       "Take one down and pass it around, 5 bottles of beer on the wall.\n"))
-
-(def song-3-0
-  (str "3 bottles of beer on the wall, 3 bottles of beer.\n"
-       "Take one down and pass it around, 2 bottles of beer on the wall.\n\n"
-       "2 bottles of beer on the wall, 2 bottles of beer.\n"
-       "Take one down and pass it around, 1 bottle of beer on the wall.\n\n"
-       "1 bottle of beer on the wall, 1 bottle of beer.\n"
-       "Take it down and pass it around, no more bottles of beer on the wall.\n\n"
-       "No more bottles of beer on the wall, no more bottles of beer.\n"
-       "Go to the store and buy some more, 99 bottles of beer on the wall.\n"))
-
 (deftest test-verse
   (testing "Testing verse..."
     (let [verse-0 "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
