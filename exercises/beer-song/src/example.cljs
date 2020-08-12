16911d4f8dd92d0e317ae270dd2f83953ae97d71
LeaveNhA <seckin.kukrer@hotmail.com>
2020-08-12 20:09:39 +0300

    Re-factored Beer Song exercise completely; Added UUID for the exercise to the exercises config file. Re-factored example source. re-factored test source.

--- exercises/beer-song/src/example.cljs
+++ exercises/beer-song/src/example.cljs
@@ -1,24 +1,44 @@
 (ns beer-song
-  (:require [clojure.string :as str]
-            [goog.string :as gstring]
+  (:require [clojure.string]
+            [goog.string]
             [goog.string.format]))
 
-(defn many [n]
-  (gstring/format "%d bottles of beer on the wall, %d bottles of beer.\nTake one down and pass it around, %d bottles of beer on the wall.\n" n n (dec n)))
+(defn- n-bottles-beer
+  "Takes bottle number and returns part of the lyrics;
+  `n bottles of beer`"
+  [^Integer n]
+  (condp = n
+        0 "no more bottles of beer"
+        1 (goog.string/format "%d bottle of beer" n)
+        (goog.string/format "%d bottles of beer" n)))
 
-(def two "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n")
-(def one "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n")
-(def zero "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n")
+(defn- first-line
+  "Takes bottle number and returns first line of the lyrics."
+  [^Integer n]
+  (clojure.string/capitalize (apply goog.string/format "%1s on the wall, %1s.\n" (repeat 2 (n-bottles-beer n)))))
 
-(defn verse [v]
-  (case (int v)
-    0 zero
-    1 one
-    2 two
-    (many v)))
+(defn- take-it-down
+  "Takes bottle number and returns part of the lyrics;
+  `Take it down and pass it around, n bottle(s) of beer on the wall.\n`"
+  [^Integer n]
+  (condp = n
+    0 (goog.string/format "Go to the store and buy some more, %s on the wall.\n" (n-bottles-beer 99))
+    1 (goog.string/format "Take it down and pass it around, %s on the wall.\n" (n-bottles-beer 0))
+    (goog.string/format "Take one down and pass it around, %s on the wall.\n" (n-bottles-beer (dec n)))))
+
+(defn verse
+  "Takes bottle number and returns verse."
+  ([n]
+   (str (first-line n)
+        (take-it-down n)))
+  ([n t]
+   (let [verse-to (range n (dec t) -1)
+         verses (map #(str (first-line %) (take-it-down %)) verse-to)]
+     (clojure.string/join "\n" verses))))
 
 (defn sing
-  ([n]   (sing n 0))
-  ([n m] (->> (range n (dec m) -1)
-              (map verse)
-              (str/join "\n"))))
+  "Gate for the verse function as test file forced..."
+  ([n t]
+   (verse n t))
+  ([n]
+   (verse n 0)))
