3c1d3aa83045b42331d9b469f06dbff9207cad04
LeaveNhA <seckin.kukrer@hotmail.com>
2020-08-14 14:29:32 +0300

    Re-factored Triangle exercise. Added UUID for the exercise to the config file. Re-factored exercise test.

--- exercises/triangle/test/triangle_test.cljs
+++ exercises/triangle/test/triangle_test.cljs
@@ -3,49 +3,58 @@
             [cljs.test :refer [deftest testing is] :as t :include-macros true]))
 
 (deftest equilateral
-  (testing "equilateral triangle"
-    (testing "all sides are equal"
+  (testing "Testing, equilateral triangle..."
+    (testing "All sides are equal"
       (is (true? (triangle/equilateral? 2 2 2))))
-    (testing "any side is unequal"
-      (is (false? (triangle/equilateral? 2 3 2))))
-    (testing "no sides are equal"
-      (is (false? (triangle/equilateral? 5 4 6))))
-    (testing "all zero sides is not a triangle"
+    (testing "Any side is unequal"
+      (is (false? (triangle/equilateral? 2 3 2)))
+      (is (false? (triangle/equilateral? 3 2 2))))
+    (testing "No sides are equal"
+      (is (false? (triangle/equilateral? 5 4 6)))
+      (is (false? (triangle/equilateral? 3 4 5))))
+    (testing "All zero sides is not a triangle"
       (is (false? (triangle/equilateral? 0 0 0))))
-    (testing "sides may be floats"
+    (testing "Sides may be floats"
+      (is (true? (triangle/equilateral? 0.9 0.9 0.9)))
       (is (true? (triangle/equilateral? 0.5 0.5 0.5))))))
 
-
 (deftest isosceles
-  (testing "isosceles triangle"
-    (testing "last two sides are equal"
-      (is (true? (triangle/isosceles? 3 4 4))))
-    (testing "first two sides are equal"
-      (is (true? (triangle/isosceles? 4 4 3))))
-    (testing "first and last sides are equal"
-      (is (true? (triangle/isosceles? 4 3 4))))
-    (testing "equilateral triangles are also isosceles"
-      (is (true? (triangle/isosceles? 4 4 4))))
-    (testing "no sides are equal"
-      (is (false? (triangle/isosceles? 2 3 4))))
-    (testing "first triangle inequality violation"
+  (testing "Testing, isosceles triangle..."
+    (testing "Last two sides are equal"
+      (is (true? (triangle/isosceles? 3 4 4)))
+      (is (true? (triangle/isosceles? 3 6 6))))
+    (testing "First two sides are equal"
+      (is (true? (triangle/isosceles? 4 4 3)))
+      (is (true? (triangle/isosceles? 5 5 3))))
+    (testing "First and last sides are equal"
+      (is (true? (triangle/isosceles? 4 3 4)))
+      (is (true? (triangle/isosceles? 6 4 6))))
+    (testing "Equilateral triangles are also isosceles"
+      (is (true? (triangle/isosceles? 4 4 4)))
+      (is (true? (triangle/isosceles? 5 5 5))))
+    (testing "No sides are equal"
+      (is (false? (triangle/isosceles? 2 3 4)))
+      (is (false? (triangle/isosceles? 4 5 6))))
+    (testing "First triangle inequality violation"
       (is (false? (triangle/isosceles? 1 1 3))))
-    (testing "second triangle inequality violation"
+    (testing "Second triangle inequality violation"
       (is (false? (triangle/isosceles? 1 3 1))))
-    (testing "third triangle inequality violation"
+    (testing "Third triangle inequality violation"
       (is (false? (triangle/isosceles? 3 1 1))))
-    (testing "sides may be floats"
-      (is (true? (triangle/isosceles? 0.5 0.4 0.5))))))
+    (testing "Sides may be floats"
+      (is (true? (triangle/isosceles? 0.5 0.4 0.5)))
+      (is (true? (triangle/isosceles? 0.4 0.5 0.4))))))
 
 (deftest scalene
-  (testing "scalene triangle"
-    (testing "no sides are equal"
-      (is (true? (triangle/scalene? 5 4 6))))
-    (testing "all sides are equal"
-      (is (false? (triangle/scalene? 4 4 4))))
-    (testing "two sides are equal"
-      (is (false? (triangle/scalene? 4 4 3))))
-    (testing "may not violate triangle inequality"
-      (is (false? (triangle/scalene? 7 3 2))))
-    (testing "sides may be floats"
-      (is (true? (triangle/scalene? 0.5 0.4 0.6))))))
+  (testing "Testing, scalene triangle..."
+    (testing "No sides are equal"
+      (is (true? (triangle/scalene? 5 4 6)))
+      (is (true? (triangle/scalene? 3 4 5))))
+    (testing "All sides are equal"
+      (is (false? (triangle/scalene? 5 5 5))))
+    (testing "Two sides are equal"
+      (is (false? (triangle/scalene? 5 5 3))))
+    (testing "May not violate triangle inequality"
+      (is (false? (triangle/scalene? 8 3 1))))
+    (testing "Sides may be floats"
+      (is (true? (triangle/scalene? 0.3 0.4 0.5))))))
