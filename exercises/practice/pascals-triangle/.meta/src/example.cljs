2d25df8332aebac0cb62ea34475de4dae372014d
Erik Schierboom <erik_schierboom@hotmail.com>
2023-05-05 15:18:55 +0200

    pascals-triangle: fix

--- exercises/practice/pascals-triangle/.meta/src/example.cljs
+++ exercises/practice/pascals-triangle/.meta/src/example.cljs
@@ -1,10 +1,13 @@
-(ns pascals-triangle)
+(ns pascals-triangle
+  "Calculates the `Pascal Triangle` in lazy way."
+  {:author [:LeaveNhA "Seçkin KÜKRER"]
+   :last-update-date "24-09-2019"})
 
-(defn- next-row [row]
-  (lazy-seq (->> (partition-all 2 1 row)
-                 (map (partial apply +'))
-                 (cons 1))))
+(defn row [r]
+  (if (vector? r)
+    (lazy-seq (cons r
+                    (row (vec (concat [1N] (map (partial apply +) (partition 2N 1N r)) [1N])))))
+    (last (take r (row [1])))))
 
-(def triangle (iterate next-row [1]))
-
-(defn row [n] (nth triangle (dec n)))
+(def triangle
+  (row [1]))
