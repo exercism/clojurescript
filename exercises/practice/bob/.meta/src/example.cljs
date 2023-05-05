da1f8ee320b67b3e39c437ddb18b2674ad925511
Erik Schierboom <erik_schierboom@hotmail.com>
2023-05-05 15:04:59 +0200

    Move examples to correct location

--- exercises/practice/bob/.meta/src/example.cljs
+++ exercises/practice/bob/.meta/src/example.cljs
@@ -1,5 +1,23 @@
-(ns bob)
+(ns bob
+  (:require [clojure.string :as str]))
 
-(defn response-for [] ;; argument goes here...
-  ;; your code goes here...
-  )
+(defn- silence?    [msg] (str/blank? msg))
+
+(defn- question?   [msg] (= \? (last msg)))
+
+(defn- has-letter? [msg] (some #(Character/isLetter (int %)) msg))
+
+(defn- shouting?   [msg] (and (= msg (str/upper-case msg))
+                              (has-letter? msg)))
+
+(defn- forceful-question? [msg]
+  (and (shouting? msg) (question? msg)))
+
+(defn response-for [input]
+  (let [clean (str/trim input)]
+    (cond
+      (forceful-question? clean) "Calm down, I know what I'm doing!"
+      (silence?  clean) "Fine. Be that way!"
+      (shouting? clean) "Whoa, chill out!"
+      (question? clean) "Sure."
+      :else             "Whatever.")))
