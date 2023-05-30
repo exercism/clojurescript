a893b4c4bb68cafec119fca0169417ba32ee774d
simisc <34340168+simisc@users.noreply.github.com>
2023-05-30 12:29:57 +0100

    Throw JS error instead of Java exception

--- exercises/practice/strain/test/strain_test.cljs
+++ exercises/practice/strain/test/strain_test.cljs
@@ -2,7 +2,7 @@
   (:require [cljs.test :refer [deftest is] :as t :include-macros true]
             [strain :refer [retain discard]]))
 
-(defn- fn-throw-exception [msg] (fn [& _] (throw (Exception. msg))))
+(defn- fn-throw-exception [msg] (fn [& _] (throw (js/Error. msg))))
 
 (deftest empty-sequence
   (is (empty? (retain even? '()))))
